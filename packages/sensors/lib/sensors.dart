30fdac8ffa8d33b8ceb2011e7a1abf4affc2b10f
Rodo <rodolfohansschmitt@gmail.com>
2021-01-18 09:08:52 -0600

    Added timestamp to output string

--- packages/sensors/lib/sensors.dart
+++ packages/sensors/lib/sensors.dart
@@ -19,8 +19,8 @@ const EventChannel _gyroscopeEventChannel =
 /// simply, you can use accelerometer readings to tell if the device is moving in
 /// a particular direction.
 class AccelerometerEvent {
-  /// Contructs an instance with the given [x], [y], and [z] values.
-  AccelerometerEvent(this.x, this.y, this.z);
+  /// Contructs an instance with the given [x], [y], [z], and [timestamp] values.
+  AccelerometerEvent(this.x, this.y, this.z, this.timestamp);
 
   /// Acceleration force along the x axis (including gravity) measured in m/s^2.
   ///
@@ -42,15 +42,19 @@ class AccelerometerEvent {
   /// towards the user and negative mean it is moving away from them.
   final double z;
 
+  /// The time in milliseconds at which the event happened.
+  /// The timestamp is the amount of time in milliseconds since the device booted.
+  final double timestamp;
+
   @override
-  String toString() => '[AccelerometerEvent (x: $x, y: $y, z: $z)]';
+  String toString() => '[AccelerometerEvent (x: $x, y: $y, z: $z) @ $timestamp]';
 }
 
 /// Discrete reading from a gyroscope. Gyroscopes measure the rate or rotation of
 /// the device in 3D space.
 class GyroscopeEvent {
   /// Contructs an instance with the given [x], [y], and [z] values.
-  GyroscopeEvent(this.x, this.y, this.z);
+  GyroscopeEvent(this.x, this.y, this.z, this.timestamp);
 
   /// Rate of rotation around the x axis measured in rad/s.
   ///
@@ -74,8 +78,12 @@ class GyroscopeEvent {
   /// on.
   final double z;
 
+  /// The time in milliseconds at which the event happened.
+  /// The timestamp is the amount of time in milliseconds since the device booted.
+  final double timestamp;
+
   @override
-  String toString() => '[GyroscopeEvent (x: $x, y: $y, z: $z)]';
+  String toString() => '[GyroscopeEvent (x: $x, y: $y, z: $z) @ $timestamp]';
 }
 
 /// Like [AccelerometerEvent], this is a discrete reading from an accelerometer
@@ -83,7 +91,7 @@ class GyroscopeEvent {
 /// [AccelerometerEvent], this event does not include the effects of gravity.
 class UserAccelerometerEvent {
   /// Contructs an instance with the given [x], [y], and [z] values.
-  UserAccelerometerEvent(this.x, this.y, this.z);
+  UserAccelerometerEvent(this.x, this.y, this.z, this.timestamp);
 
   /// Acceleration force along the x axis (excluding gravity) measured in m/s^2.
   ///
@@ -105,20 +113,24 @@ class UserAccelerometerEvent {
   /// towards the user and negative mean it is moving away from them.
   final double z;
 
+  /// The time in milliseconds at which the event happened.
+  /// The timestamp is the amount of time in milliseconds since the device booted.
+  final double timestamp;
+
   @override
-  String toString() => '[UserAccelerometerEvent (x: $x, y: $y, z: $z)]';
+  String toString() => '[UserAccelerometerEvent (x: $x, y: $y, z: $z) @ $timestamp]';
 }
 
 AccelerometerEvent _listToAccelerometerEvent(List<double> list) {
-  return AccelerometerEvent(list[0], list[1], list[2]);
+  return AccelerometerEvent(list[0], list[1], list[2], list[3]);
 }
 
 UserAccelerometerEvent _listToUserAccelerometerEvent(List<double> list) {
-  return UserAccelerometerEvent(list[0], list[1], list[2]);
+  return UserAccelerometerEvent(list[0], list[1], list[2], list[3]);
 }
 
 GyroscopeEvent _listToGyroscopeEvent(List<double> list) {
-  return GyroscopeEvent(list[0], list[1], list[2]);
+  return GyroscopeEvent(list[0], list[1], list[2], list[3]);
 }
 
 Stream<AccelerometerEvent> _accelerometerEvents;
